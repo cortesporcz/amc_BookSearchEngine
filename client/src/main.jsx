aae05f738c131fbc864dd0f1dfb1d8edfff17dbe
Angelina Cortes <cortesporcz@gmail.com>
2024-04-24 18:49:43 -0500

    adding comments to make sense of it all

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -2,10 +2,12 @@ import ReactDOM from 'react-dom/client'
 import { createBrowserRouter, RouterProvider } from 'react-router-dom'
 import 'bootstrap/dist/css/bootstrap.min.css'
 
+// Importing the main 'App' component and other page components
 import App from './App.jsx'
 import SearchBooks from './pages/SearchBooks'
 import SavedBooks from './pages/SavedBooks'
 
+// Creating a browser router with route configurations
 const router = createBrowserRouter([
   {
     path: '/',
@@ -16,8 +18,8 @@ const router = createBrowserRouter([
         index: true,
         element: <SearchBooks />
       }, {
-        path: '/saved',
-        element: <SavedBooks />
+        path: '/saved', // Nested route for '/saved'
+        element: <SavedBooks /> // Component to render for the '/saved' route
       }
     ]
   }
