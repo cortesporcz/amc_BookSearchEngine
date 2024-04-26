401889b552e9c59a20318432d93e9ee855fe9389
Angelina Cortes <cortesporcz@gmail.com>
2024-04-25 22:44:00 -0500

    updating file with routes and path from starter code

--- server/routes/index.js
+++ server/routes/index.js
@@ -1,12 +1,27 @@
+//const router = require('express').Router();
+//import { join } from 'path';
+//import apiRoutes from './api';
+
+//router.use('/api', apiRoutes);
+
+// serve up react front-end in production
+//router.use((req, res) => {
+  //res.sendFile(join(__dirname, '../../client/build/index.html'));});
+
+//export default router;
+
 const router = require('express').Router();
-import { join } from 'path';
-import apiRoutes from './api';
+const path = require('path');
+const apiRoutes = require('./api');
 
 router.use('/api', apiRoutes);
 
-// serve up react front-end in production
-router.use((req, res) => {
-  res.sendFile(join(__dirname, '../../client/build/index.html'));
+// Serve React static files
+router.use(express.static(path.resolve(__dirname, '../../client/build')));
+
+// For all other routes, serve the React app
+router.get('*', (req, res) => {
+  res.sendFile(path.resolve(__dirname, '../../client/build', 'index.html'));
 });
 
-export default router;
+module.exports = router;
