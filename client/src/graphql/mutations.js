07a255731a77971a58a046847e9c54d7d92527da
Angelina Cortes <cortesporcz@gmail.com>
2024-04-25 23:09:17 -0500

    changing mutation file syntax from create user to login

--- client/src/graphql/mutations.js
+++ client/src/graphql/mutations.js
@@ -1,56 +1,84 @@
 // Importing the gql function from the '@apollo/client' library //
 import { gql } from '@apollo/client';
 
+export const LOGIN_USER = gql`
+    mutation login($email: String!, $password: String!) {
+        login(email: $email, password: $password) {
+            token
+            user {
+                _id
+                username
+                email
+                bookCount
+                savedBooks {
+                    bookId
+                    authors
+                    description
+                    title
+                    image
+                    link
+                }
+            }
+        }
+    }
+`;
+
 export const ADD_USER = gql`
-  mutation createUser($username: String!, $email: String!, $password: String!) {
-    createUser(username: $username, email: $email, password: $password) {
-      token ## JWT token returned by the server
-      user {
-        _id ## User ID
-        username ## User's username
-      }
-    }
-  }
-`;
-
-export const LOGIN = gql`
-  mutation login($email: String!, $password: String!) {
-    login(email: $email, password: $password) {
-        token ## JWT token returned by the server
-        user {
-            _id ## User ID
-            username ## User's username
+    mutation addUser($username: String!, $email: String!, $password: String!) {
+        addUser(username: $username, email: $email, password: $password) {
+            token
+            user {
+                _id
+                username
+                email
+                bookCount
+                savedBooks {
+                    bookId
+                    authors
+                    description
+                    title
+                    image
+                    link
+                }
+            }
         }
     }
-  }
 `;
 
-export const ADD_BOOK = gql`
-  mutation saveBook($content: BookArrayInput!) {
-    saveBook(content: $content) {
-        savedBooks {
-          authors ## Book authors
-          description ## Book description
-          bookId ## Book ID
-          image ## Book image
-          link ## Book link
-          title ## Book title
+export const SAVE_BOOK = gql`
+    mutation saveBook($input: BookInput!) {
+        saveBook(input: $input) {
+            _id
+            username
+            email
+            bookCount
+            savedBooks {
+                bookId
+                authors
+                description
+                title
+                image
+                link
+            }
         }
     }
-  }
 `;
 
-export const DELETE_BOOK = gql`
-  mutation deleteBook($bookId: String!) {
-    deleteBook(bookId: $bookId) {
-        savedBooks {
-          authors ## Book authors
-          description ## Book description
-          bookId ## Book ID
-          image ## Book image
-          link ## Book link
-          title ## Book title
+export const REMOVE_BOOK = gql`
+    mutation removeBook($bookId: ID!) {
+        removeBook(bookId: $bookId) {
+            _id
+            username
+            email
+            bookCount
+            savedBooks {
+                bookId
+                authors
+                description
+                title
+                image
+                link
+            }
         }
     }
-  }
 `;
