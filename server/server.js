f98e78b22b9a8f8c26629a9c3d0ac3a9b51ede86
Angelina Cortes <cortesporcz@gmail.com>
2024-04-25 20:38:11 -0500

    fixing errors on server.js

--- server/server.js
+++ server/server.js
@@ -1,4 +1,4 @@
-//Importing mods and dependencies
+// Importing necessary modules and dependencies
 const express = require('express');
 const path = require('path');
 const db = require('./config/connection');
@@ -8,28 +8,50 @@ const { expressMiddleware } = require('@apollo/server/express4');
 const { authMiddleware } = require('./utils/auth');
 const { typeDefs, resolvers } = require('./schemas');
 
-//Creating express app
+// Creating an Express application
 const app = express();
 
-//Setting port for server
+// Setting up the port for the server, defaulting to 3001
 const PORT = process.env.PORT || 3001;
 
-//Setting up the Apollo Server with provided type definitions and resolvers
+// Setting up the Apollo Server with provided type definitions and resolvers
 const server = new ApolloServer({
   typeDefs,
   resolvers,
 });
 
-app.use(express.urlencoded({ extended: true }));
-app.use(express.json());
+// Asynchronous function to start Apollo Server and configure Express
+const ServerOfApollo = async () => {
+  // Starting Apollo Server asynchronously before other configurations
+  await server.start();
 
-// if we're in production, serve client/build as static assets
-if (process.env.NODE_ENV === 'production') {
-  app.use(express.static(path.join(__dirname, '../client/build')));
-}
+  // Configuring middleware for parsing URL-encoded and JSON data
+  app.use(express.urlencoded({ extended: true }));
+  app.use(express.json());
 
-app.use(routes);
+  // Configuring the main route for Apollo Server, using authMiddleware for decoding JWT tokens
+  app.use('/graphql', expressMiddleware(server, {
+    context: authMiddleware,
+  }));
 
-db.once('open', () => {
-  app.listen(PORT, () => console.log(`🌍 Now listening on localhost:${PORT}`));
-});
+  // Serving static assets and the client build in production
+  if (process.env.NODE_ENV === 'production') {
+    app.use(express.static(path.join(__dirname, '../client/dist')));
+
+    // Handling all other routes by serving the index.html file
+    app.get('*', (req, res) => {
+      res.sendFile(path.join(__dirname, '../client/dist/index.html'));
+    });
+  }
+
+  // Don't need to use these routes anymore as the app is converted from RESTful to GraphQL
+  // app.use(routes);
+
+  // Connecting to MongoDB and starting the Express server once the database is open
+  db.once('open', () => {
+    app.listen(PORT, () => console.log(`Now listening on localhost:${PORT}`));
+  });
+};
+
+// Calling the asynchronous function to start the Apollo Server and configure Express
+ServerOfApollo();
